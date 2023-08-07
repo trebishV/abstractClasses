0ff2b48fbdeacb7cb71f3b07e5822146d71ffc28
Trebish Vladimir <v.trebish@gmail.com>
2023-08-08 01:21:24 +0300

    implementing Preson for Eml

--- com/trebish/src/com/trebish/AbstractClasses/Employe.java
+++ com/trebish/src/com/trebish/AbstractClasses/Employe.java
@@ -1,5 +1,16 @@
 package com.trebish.AbstractClasses;
 
-public class Employe {
+public class Employe extends Person{
+
+	public Employe(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getDescriptionOfPerson() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
