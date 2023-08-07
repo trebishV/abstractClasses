dd01f176d1597d376314706925a454443c7ea351
Trebish Vladimir <v.trebish@gmail.com>
2023-08-08 01:16:07 +0300

    add 2 classes Eml and Man

---
+++ com/trebish/src/com/trebish/AbstractClasses/Manager.java
@@ -0,0 +1,16 @@
+package com.trebish.AbstractClasses;
+
+public class Manager extends Person {
+
+	public Manager(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getDescriptionOfPerson() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
