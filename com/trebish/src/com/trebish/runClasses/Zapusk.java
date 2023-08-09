dbeb8ad039bc99db6287af9a0eb0684683eec592
Trebish Vladimir <v.trebish@gmail.com>
2023-08-10 00:48:41 +0300

    add start class

---
+++ com/trebish/src/com/trebish/runClasses/Zapusk.java
@@ -0,0 +1,24 @@
+
+package com.trebish.runClasses;
+
+import java.util.ArrayList;
+
+import com.trebish.AbstractClasses.*;
+
+public class Zapusk {
+	
+	//cheli.add(new Employe("Gusdabzhak", 3));
+	
+	public static void main(String[] args) {
+		ArrayList<Person> cheli = new ArrayList<Person>();
+		
+		
+		Person p1 = new Employe("Gubuzduk", 3);
+		Person p2 = new Manager("Uzukrab",3440.20);
+		cheli.add(p2);
+		cheli.add(p1);
+		for (Person p: cheli) System.out.println(p);
+
+	}
+
+}
